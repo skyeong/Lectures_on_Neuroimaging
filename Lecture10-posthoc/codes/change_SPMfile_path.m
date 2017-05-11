5a5df6051d3d5b46f5a58d5dbcff47a18ec8f4b9
Sunghyon Kyeong <sunghyon.kyeong@gmail.com>
2017-05-11 15:27:24 +0900

    제 10장. 강의노트 업데이트

--- Lecture10-posthoc/codes/change_SPMfile_path.m
+++ Lecture10-posthoc/codes/change_SPMfile_path.m
@@ -8,7 +8,8 @@ SPM.swd = targetdir;
 nfile = length(SPM.xY.VY);
 for j=1:nfile,
     [p1, f1, e1]=fileparts(SPM.xY.VY(j).fname);
-    SPM.xY.VY(j).fname=fullfile(swa_path, [f1, e1]);
+    fmridirs = strsplit(p1,'/');
+    SPM.xY.VY(j).fname=fullfile(swa_path, fmridirs{end}, [f1, e1]);
 end
 save(SPMfile,'SPM');
 % ---------------------------------
